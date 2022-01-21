3cdd457639abb1a6f487c8de284b7f4a74b25429
Guo <espana1210@gmail.com>
2022-01-21 13:24:19 -0800

    first commit

---
+++ partII.js
@@ -0,0 +1,32 @@
+//part I
+/*
+mkdir first
+cd first
+touch person.txt
+mv person.txt another.txt
+cp another.txt copy.txt
+rm copy.txt
+cp -r first second
+rm -rf second
+
+*/
+
+//part II
+/*
+1.man command lists the manual for a command
+2.-I lists files in a list format with more detail
+-a lists all files and folders including hidden ones
+3.Option/Alt + left/right(jump between words)
+4.Command/Conrol + e (move to end of line)
+5.Command/Control + a (move to beginning of line)
+6.Command/Control + w (delete the word where your cursor is)
+7. a terminal refers to a wrapper program which runs a shell
+The shell is the program which actually processes commands and returns output
+8.absolute path: cd/Users/colt/Desktop starting from the root(first/, then Users...)
+9.relative path: cd Desktop 
+10.flag can change and even enhance commands and are added using a - after the command
+-a -I for ls command
+-r for cp and rm
+11.-r is called a _flag_ , as an option that can be passed to that command to remove file/folder
+-f will force removal
+*/
